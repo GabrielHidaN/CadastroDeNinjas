93a8a3308ed561adbf2854e3270b0db26358ca67
GabrielHidaN <victorbielgv7@gmail.com>
2025-08-03 01:12:22 -0300

    feat: create DTO and Mapper for Mission entity

---
+++ src/main/java/dev/gabriel/CadastroDeNinjas/missions/dto/MissionDTO.java
@@ -0,0 +1,4 @@
+package dev.gabriel.CadastroDeNinjas.missions.dto;
+
+public class MissionDTO {
+}
---
+++ src/main/java/dev/gabriel/CadastroDeNinjas/missions/mapper/MissionMapper.java
@@ -0,0 +1,4 @@
+package dev.gabriel.CadastroDeNinjas.missions.mapper;
+
+public class MissionMapper {
+}
