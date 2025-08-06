9fb12e61fc11b86805ca34b138301f5e85e023cc
GabrielHidaN <victorbielgv7@gmail.com>
2025-08-06 00:44:19 -0300

    fix: Remove the responsibility from my model and move it into my DTO, using Streams in the Mapper.

--- src/main/java/dev/gabriel/CadastroDeNinjas/missions/mapper/MissionMapper.java
+++ src/main/java/dev/gabriel/CadastroDeNinjas/missions/mapper/MissionMapper.java
@@ -1,4 +1,34 @@
 package dev.gabriel.CadastroDeNinjas.missions.mapper;
 
+import dev.gabriel.CadastroDeNinjas.missions.dto.MissionDTO;
+import dev.gabriel.CadastroDeNinjas.missions.model.MissionModel;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class MissionMapper {
+
+    public MissionModel map(MissionDTO missionDTO){
+        MissionModel missionModel = new MissionModel();
+
+        missionModel.setId(missionDTO.getId());
+        missionModel.setName(missionDTO.getName());
+        missionModel.setDifficulty(missionDTO.getDifficulty());
+        missionModel.setNinjas(missionDTO.getNinjas());
+
+        return missionModel;
+    }
+
+    public  static MissionDTO map(MissionModel missionModel){
+        MissionDTO missionDTO = new MissionDTO();
+
+        missionDTO.setId(missionModel.getId());
+        missionDTO.setName(missionModel.getName());
+        missionDTO.setDifficulty(missionModel.getDifficulty());
+        missionDTO.setNinjas(missionModel.getNinjas());
+
+        return missionDTO;
+    }
+
+
 }
