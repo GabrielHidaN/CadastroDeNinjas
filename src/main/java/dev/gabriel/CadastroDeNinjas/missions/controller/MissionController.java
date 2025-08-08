c86ad1eb5ee4c85d5994f640c1563fc69b740439
GabrielHidaN <victorbielgv7@gmail.com>
2025-08-08 17:15:59 -0300

    doc: Add Swagger to my application to document the routes in the Mission  controller.

--- src/main/java/dev/gabriel/CadastroDeNinjas/missions/controller/MissionController.java
+++ src/main/java/dev/gabriel/CadastroDeNinjas/missions/controller/MissionController.java
@@ -3,6 +3,10 @@ import dev.gabriel.CadastroDeNinjas.missions.dto.MissionDTO;
 import dev.gabriel.CadastroDeNinjas.missions.model.MissionModel;
 import dev.gabriel.CadastroDeNinjas.missions.service.MissionService;
 import dev.gabriel.CadastroDeNinjas.ninja.dto.NinjaDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +26,17 @@ public class MissionController {
 
 
     @PostMapping("/create")
-    public ResponseEntity<String> createMission(@RequestBody MissionDTO mission){
+    @Operation(summary = "Creates a new mission" ,
+            description = "Route creates a new mission and inserts into the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Mission created"),
+            @ApiResponse(responseCode = "400" ,
+                    description = "Mission Creation Error")
+    })
+    public ResponseEntity<String> createMission(
+            @Parameter(description = "User sends the ninja data to be updated in the body of the request")
+            @RequestBody MissionDTO mission){
 
         MissionDTO newMission = missionService.createMission(mission);
 
@@ -33,10 +47,18 @@ public class MissionController {
     }
 
 
-
-
     @GetMapping("/{id}")
-    public ResponseEntity<?> showMissionById(@PathVariable Long id){
+    @Operation(summary = "Show Mission By Id" ,
+            description = "Route show a mission by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Mission found"),
+            @ApiResponse(responseCode = "404" ,
+                    description = "Mission Not found")
+    })
+    public ResponseEntity<?> showMissionById(
+            @Parameter(description = "The user provides the ID along the path of the route")
+            @PathVariable Long id){
 
         MissionDTO missionById = missionService.showMissionById(id);
         if (missionById != null){
@@ -48,9 +70,9 @@ public class MissionController {
     }
 
 
-
-    // Show all Missions (READ)
     @GetMapping("/all")
+    @Operation(summary = "Show all Missions" ,
+            description = "Route show all missions")
     public ResponseEntity<List<MissionDTO>> showAllMissions(){
         List<MissionDTO> allMissions =  missionService.showAllMissions();
 
@@ -58,9 +80,20 @@ public class MissionController {
     }
 
 
-
     @PutMapping("/update/{id}")
-    public ResponseEntity<?> updateMission(@PathVariable Long id , @RequestBody MissionDTO mission){
+    @Operation(summary = "Update the Mission" ,
+            description = "Route updates a mission and inserts changes in the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Mission updated"),
+            @ApiResponse(responseCode = "404" ,
+                    description = "Mission error not found, it was not possible to make the changes")
+    })
+    public ResponseEntity<?> updateMission(
+            @Parameter(description = "The user provides the ninja ID who wants to update along the path of the route")
+            @PathVariable Long id ,
+            @Parameter(description = "User sends the ninja data to be updated in the body of the request")
+            @RequestBody MissionDTO mission){
 
         MissionDTO missionUpdate = missionService.updateMission(id , mission);
 
@@ -74,9 +107,18 @@ public class MissionController {
     }
 
 
-
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> missionDelete(@PathVariable  Long id){
+    @Operation(summary = "Delete the Mission" ,
+            description = "Route excludes a mission and removes in the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Mission updated"),
+            @ApiResponse(responseCode = "404",
+                    description = "Mission error not found")
+    })
+    public ResponseEntity<String> missionDelete(
+            @Parameter(description = "The user provides the ID along the path of the route")
+            @PathVariable  Long id){
 
         MissionDTO mission = missionService.showMissionById(id);
 
